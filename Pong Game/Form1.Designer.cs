9c0ca48574ba8876fed4816d88ea84746dc58ea6
Manungku <manungku@students.amikom.ac.id>
2023-06-16 12:48:38 +0700

    this is public code

---
+++ Pong Game/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Pong_Game
+{
+    partial class pong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.cpuPlayer = new System.Windows.Forms.PictureBox();
+            this.pongBall = new System.Windows.Forms.PictureBox();
+            this.playerScoreLabel = new System.Windows.Forms.Label();
+            this.cpuScoreLabel = new System.Windows.Forms.Label();
+            this.pongTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pongBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.player1.Location = new System.Drawing.Point(12, 246);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(24, 166);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            // 
+            // cpuPlayer
+            // 
+            this.cpuPlayer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cpuPlayer.Location = new System.Drawing.Point(1077, 246);
+            this.cpuPlayer.Name = "cpuPlayer";
+            this.cpuPlayer.Size = new System.Drawing.Size(24, 166);
+            this.cpuPlayer.TabIndex = 1;
+            this.cpuPlayer.TabStop = false;
+            // 
+            // pongBall
+            // 
+            this.pongBall.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pongBall.Location = new System.Drawing.Point(546, 309);
+            this.pongBall.Name = "pongBall";
+            this.pongBall.Size = new System.Drawing.Size(20, 20);
+            this.pongBall.TabIndex = 2;
+            this.pongBall.TabStop = false;
+            // 
+            // playerScoreLabel
+            // 
+            this.playerScoreLabel.AutoSize = true;
+            this.playerScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerScoreLabel.Location = new System.Drawing.Point(268, 28);
+            this.playerScoreLabel.Name = "playerScoreLabel";
+            this.playerScoreLabel.Size = new System.Drawing.Size(51, 55);
+            this.playerScoreLabel.TabIndex = 3;
+            this.playerScoreLabel.Text = "0";
+            // 
+            // cpuScoreLabel
+            // 
+            this.cpuScoreLabel.AutoSize = true;
+            this.cpuScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpuScoreLabel.Location = new System.Drawing.Point(777, 28);
+            this.cpuScoreLabel.Name = "cpuScoreLabel";
+            this.cpuScoreLabel.Size = new System.Drawing.Size(51, 55);
+            this.cpuScoreLabel.TabIndex = 4;
+            this.cpuScoreLabel.Text = "0";
+            // 
+            // pongTimer
+            // 
+            this.pongTimer.Enabled = true;
+            this.pongTimer.Interval = 20;
+            this.pongTimer.Tick += new System.EventHandler(this.pongTimer_Tick);
+            // 
+            // pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 656);
+            this.Controls.Add(this.cpuScoreLabel);
+            this.Controls.Add(this.playerScoreLabel);
+            this.Controls.Add(this.pongBall);
+            this.Controls.Add(this.cpuPlayer);
+            this.Controls.Add(this.player1);
+            this.Name = "pong";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pong_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.pong_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pongBall)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox cpuPlayer;
+        private System.Windows.Forms.PictureBox pongBall;
+        private System.Windows.Forms.Label playerScoreLabel;
+        private System.Windows.Forms.Label cpuScoreLabel;
+        private System.Windows.Forms.Timer pongTimer;
+    }
+}
+
---
+++ Pong Game/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong_Game
+{
+    public partial class pong : Form
+    {
+        //int cpuDirection = 5;
+        int ballXCoordinate = 5;
+        int ballYCoordinate = 5;
+        int playerScore = 0;
+        int cpuScore = 0;
+        int bottomBoundary;
+        int centerPoint;
+        int xMidpoint;
+        int yMidpoint;
+        bool playerDetectedUp;
+        bool playerDetectedDown;
+        bool cpuDetectedUp;
+        bool cpuDetectedDown;
+        //int spaceBarClicked = 0;
+        
+        public pong()
+        {
+            InitializeComponent();
+            bottomBoundary = ClientSize.Height - player1.Height;
+            xMidpoint = ClientSize.Width / 2;
+            yMidpoint = ClientSize.Height / 2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pongTimer_Tick(object sender, EventArgs e)
+        {
+            Random newBallSpot = new Random();
+            int newSpot = newBallSpot.Next(100, ClientSize.Height - 100);
+            pongBall.Top -= ballYCoordinate;
+            pongBall.Left -= ballXCoordinate;
+            //cpuPlayer.Top += cpuDirection;
+            //if(cpuPlayer.Top < 0 || cpuPlayer.Top > bottomBoundary) { cpuDirection = -cpuDirection; }
+            if (pongBall.Left < 0)
+            {
+                pongBall.Left = xMidpoint;
+                pongBall.Top = newSpot;
+                ballYCoordinate = -ballXCoordinate;
+                cpuScore++;
+                cpuScoreLabel.Text = cpuScore.ToString();
+            }
+            if(pongBall.Left + pongBall.Width > ClientSize.Width)
+            {
+                pongBall.Left = xMidpoint;
+                pongBall.Top = newSpot;
+                ballYCoordinate = -ballXCoordinate;
+                playerScore++;
+                playerScoreLabel.Text = playerScore.ToString();
+            }
+            if(pongBall.Top < 0 || pongBall.Top + pongBall.Height > ClientSize.Height) { ballYCoordinate = -ballYCoordinate; }
+            if(pongBall.Bounds.IntersectsWith(player1.Bounds) || pongBall.Bounds.IntersectsWith(cpuPlayer.Bounds)) 
+            {
+                ballXCoordinate = -ballXCoordinate;
+            }
+            if(playerDetectedUp == true && player1.Top > 0) { player1.Top -= 10; }
+            if(playerDetectedDown == true && player1.Top < bottomBoundary) { player1.Top += 10; }
+            if (cpuDetectedUp == true && cpuPlayer.Top > 0) { cpuPlayer.Top -= 10; }
+            if (cpuDetectedDown == true && cpuPlayer.Top < bottomBoundary) { cpuPlayer.Top += 10; }
+            if (playerScore >= 10) { pongTimer.Stop(); }
+        }
+
+        private void pong_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W) { playerDetectedUp = false; }
+            if (e.KeyCode == Keys.S) { playerDetectedDown = false ; }
+            if (e.KeyCode == Keys.Up) { cpuDetectedUp = false; }
+            if (e.KeyCode == Keys.Down) { cpuDetectedDown = false; }
+        }
+
+        private void pong_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.W) { playerDetectedUp = true; }
+            if (e.KeyCode == Keys.S) { playerDetectedDown = true; }
+            if (e.KeyCode == Keys.Up) { cpuDetectedUp = true; }
+            if (e.KeyCode == Keys.Down) { cpuDetectedDown = true; }
+            //if (e.KeyCode == Keys.Space)
+            //{
+                //pongTimer.Stop();
+            //}
+            //else
+            //{
+               // pongTimer.Start();
+            //}
+            //spaceBarClicked++;
+        }
+
+       
+    }
+}
